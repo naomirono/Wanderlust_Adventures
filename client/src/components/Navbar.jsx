983cd30e1d1b6c17776a1fa98ebc19016dbee0a2
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-02 15:37:32 +0300

    Implement search bar functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { FiSearch, FiCheck } from 'react-icons/fi';
 
 const Navbar = () => {
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleTyping = (e) => {
+    setIsTyping(e.target.value !== ''); 
+  };
+
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -17,17 +24,27 @@ const Navbar = () => {
               <li><a href="#" className="text-white">Blog</a></li>
             </ul>
         </div>
-        <div className="flex items-center">
-          <div className="mr-4">
-            <input type="text" placeholder="Search" className="p-2 rounded" />
+        <div className="relative flex items-center">
+          <div className="absolute left-0 pl-2 top-1/2 transform -translate-y-1/2 text-gray-400">
+            <div className={isTyping ? 'bg-gray-500 text-white p-1 rounded-full text-xl' : ''}>
+              {isTyping ? <FiCheck /> : <FiSearch />}
+            </div>
           </div>
-          <div className="flex items-center space-x-2">
-            <a href="#" className="text-white"><FaFacebook /></a>
-            <a href="#" className="text-white"><FaInstagram /></a>
-            <a href="#" className="text-white"><FaTwitter /></a>
-            <a href="#" className="text-white"><FaLinkedin /></a>
+          <input
+            type="text"
+            placeholder="Search"
+            className="text-gray-500 p-2 pl-10 rounded-3xl w-40 md:w-80 mr-6"
+            onChange={handleTyping}
+          />
+
+          <div className="flex items-center space-x-4">
+            <a href="#" className="text-white"><FaFacebook className="text-xl"/></a>
+            <a href="#" className="text-white"><FaInstagram className="text-xl"/></a>
+            <a href="#" className="text-white"><FaTwitter className="text-xl"/></a>
+            <a href="#" className="text-white"><FaLinkedin className="text-xl"/></a>
           </div>
         </div>
+        
       </div>
     </nav>
   );
