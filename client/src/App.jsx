763e8ba746debb22c265d4d1c78ff623c440d4e8
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-02 20:07:25 +0300

    Import the hero section and render it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import bgImage from '../src/assets/beautiful.jpg';
+import HeroSection from './components/Hero';
 
 function App() {
   return (
     <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${bgImage})` }}>
     <div className='bg-black bg-opacity-20'>
        <Navbar />
+       <HeroSection/>
     </div> 
     </div>
   );
