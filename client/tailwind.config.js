e5a6dc8b4c5a49a05ee6d4f8694fee2777d589d0
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-02 20:04:55 +0300

    Add custom colors to tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        turquoise: {
+          '500': '#4DB6AC',
+          '600': '#3F9F93',
+          '700': '#357F72',
+          '800': '#2C665E',
+          '900': '#214C49',
+        },
+      },
+    },
   },
   plugins: [],
 }
