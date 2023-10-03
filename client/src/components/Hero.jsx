c6b82ed2e4fb5d22e9be96334e601648703a6980
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-03 14:11:58 +0300

    Import styles.css

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import Slider from 'react-slick';
+import '../style.css'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import card1 from '../assets/card1.jpg';
