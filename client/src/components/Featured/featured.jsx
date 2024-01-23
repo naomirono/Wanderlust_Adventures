c728b9e59a0bdc0b3bd7f01b47028e640c27c48d
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-24 02:23:44 +0300

    Add a text color to icons

--- client/src/components/Featured/featured.jsx
+++ client/src/components/Featured/featured.jsx
@@ -6,31 +6,31 @@ const Features = () => {
       <div className="flex justify-around items-center">
 
         <div className="text-center">
-          <i className="fa-solid fa-globe text-blue-500 text-4xl mb-2"></i>
+          <i className="fa-solid fa-globe text-orange-500 text-4xl mb-2"></i>
           <p className="text-sm font-semibold">Global</p>
           <p className="text-sm font-semibold">Destinations</p>
         </div>
 
         <div className="text-center">
-          <i className="fa-solid fa-money-check-dollar text-blue-500 text-4xl mb-2"></i>
+          <i className="fa-solid fa-money-check-dollar text-orange-500 text-4xl mb-2"></i>
           <p className="text-sm font-semibold">Transparent</p>
           <p className="text-sm font-semibold">Pricing</p>
         </div>
 
         <div className="text-center">
-          <i className="fa-solid fa-face-smile text-blue-500 text-4xl mb-2"></i>
+          <i className="fa-solid fa-face-smile text-orange-500 text-4xl mb-2"></i>
           <p className="text-sm font-semibold">100% Satisfaction</p>
           <p className="text-sm font-semibold">Guarantee</p>
         </div>
 
         <div className="text-center">
-          <i className="fa-solid fa-user-tie text-blue-500 text-4xl mb-2"></i>
+          <i className="fa-solid fa-user-tie text-orange-500 text-4xl mb-2"></i>
           <p className="text-sm font-semibold">Professional & </p>
           <p className="text-sm font-semibold">Qualified</p>
         </div>
 
         <div className="text-center">
-          <i className="fa-solid fa-laptop text-blue-500 text-4xl mb-2"></i>
+          <i className="fa-solid fa-laptop text-orange-500 text-4xl mb-2"></i>
           <p className="text-sm font-semibold">Online</p>
           <p className="text-sm font-semibold">Booking</p>
         </div>
