95e2d06b13fa881b242ec13fdfe41be44cd00063
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-23 23:53:13 +0300

    import about component and render it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react';
-import bgImage from './assets/bg3.jpg';
-import HeroSection from './components/Hero/Hero';
-import Navbar from "./components/Navbar/Navbar";
+import HomeHero from "./components/HomeHero/HomeHero";
+import About from "./components/about/about";
 
 function App() {
-  const [backgroundImage, setBackgroundImage] = useState(bgImage);
-
+  
   return (
-    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className='bg-black bg-opacity-40'>
-        <Navbar />
-        <HeroSection setBgImage={setBackgroundImage} />
-      </div>
+    <div>
+      <HomeHero/>
+      <About/>
     </div>
   );
 }
