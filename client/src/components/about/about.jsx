86fd1f8ecc8c23377225f9829a59c6fd15ec321d
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-23 23:51:35 +0300

    Add font family to titles and paragraph

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -12,27 +12,27 @@ const About = () => {
 
         {/* Right Section */}
         <div className="w-1/2 ml-8">
-          <h4 className="text-blue-500 text-xl font-extrabold mb-4">WHO WE ARE</h4>
-          <h2 className="text-3xl font-bold mb-2">Real People Delivering Real Results</h2>
-          <p className="mb-4">
+          <h4 className="text-blue-500 text-xl font-semibold mb-4 font-fira">WHO WE ARE</h4>
+          <h2 className="text-4xl font-semibold font-fira mb-2">Real People Delivering Real Results</h2>
+          <p className="mb-4 font-inter">
           At Wanderlust, we are passionate about creating unforgettable travel experiences. Our team
             is composed of avid travelers who believe in the transformative power of exploring new places.
           </p>
 
           {/* Two Points Section */}
           <div className="flex mb-4">
-            <div className="mr-4 border-2 border-black">
-              <h3 className="text-xl font-semibold mb-2">Expert Planning</h3>
-              <p>We meticulously plan every detail of your journey to ensure a seamless and stress-free experience.</p>
+            <div className="w-1/2 mr-4 border-2 border-blue-500 p-2">
+              <h3 className="text-l font-semibold mb-2 font-fira">Expert Planning</h3>
+              <p className='text-sm font-inter'>We meticulously plan every detail of your journey.</p>
             </div>
-            <div className='border-2 border-black'>
-              <h3 className="text-xl font-semibold mb-2">Customized Packages</h3>
-              <p>Our travel packages are tailor-made to suit your preferences, ensuring a personalized adventure for you.</p>
+            <div className='w-1/2 border-2 border-blue-500 p-2'>
+              <h3 className="text-l font-semibold mb-2 font-fira">Customized Packages</h3>
+              <p className='text-sm font-inter'>Our travel packages are tailor-made to suit your preferences.</p>
             </div>
           </div>
 
           {/* Additional Paragraph */}
-          <p className="mb-4">
+          <p className="mb-4 font-inter">
           Embark on a journey with Wanderlust, where every destination is a new story waiting to be written.
             Whether you seek relaxation on pristine beaches, cultural immersion in historic cities, or thrilling
             adventures in exotic landscapes, we have the perfect itinerary for you.
