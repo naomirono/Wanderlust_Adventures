4a75f4a1c62739b28f307fbda9c6b31185778e5c
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-24 01:01:15 +0300

    Import the features component and render it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import HomeHero from "./components/HomeHero/HomeHero";
 import About from "./components/about/about";
+import Features from "./components/Featured/featured";
 
 function App() {
   
   return (
     <div>
       <HomeHero/>
+      <Features/>
       <About/>
     </div>
   );
