3be3287718c22040f642a066a73acd23c173227a
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-24 02:24:41 +0300

    Change blue color to orange

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -1,4 +1,4 @@
-import aboutImg from '../../assets/Blue_Orange.png'
+import aboutImg from '../../assets/WanderlustAbout.png'
 
 const About = () => {
   return (
@@ -11,7 +11,7 @@ const About = () => {
         </div>
 
         <div className="w-1/2 ml-8">
-          <h4 className="text-blue-500 text-xl font-semibold mb-4 font-fira">WHO WE ARE</h4>
+          <h4 className="text-orange-500 text-xl font-semibold mb-4 font-fira">WHO WE ARE</h4>
           <h2 className="text-4xl font-semibold font-fira mb-2">Real People Delivering Real Results</h2>
           <p className="mb-6 font-inter">
           At Wanderlust, we are passionate about creating unforgettable travel experiences. Our team
@@ -19,15 +19,15 @@ const About = () => {
           </p>
 
           <div className="flex mb-6">
-            <div className="w-1/2 mr-4 border-2 border-blue-500 rounded flex items-start p-1">
-              <i className="fa-solid fa-list-check text-4xl pt-4 text-blue-500"></i>
+            <div className="w-1/2 mr-4 border-2 border-orange-500 rounded-[8px] flex items-start p-2">
+              <i className="fa-solid fa-list-check text-4xl pt-4 text-orange-500"></i>
               <div className='p-2'>
                 <h3 className="text-l font-semibold font-fira">Expert Planning</h3>
                 <p className="text-sm font-inter">We meticulously plan every detail of your journey.</p>
               </div>
             </div>
-            <div className='w-1/2 border-2 border-blue-500 rounded flex items-start p-1'>
-              <i className="fa-solid fa-suitcase text-4xl pt-4 text-blue-500"></i>
+            <div className='w-1/2 border-2 border-orange-500 rounded-[8px] flex items-start p-2'>
+              <i className="fa-solid fa-suitcase text-4xl pt-4 text-orange-500"></i>
               <div className='p-2'>
                <h3 className="text-l font-semibold font-fira">Customized Packages</h3>
                <p className='text-sm font-inter'>Our travel packages are tailor-made to suit your preferences.</p>
@@ -41,8 +41,8 @@ const About = () => {
             adventures in exotic landscapes, we have the perfect itinerary for you.
           </p>
 
-          <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 font-fira">
-            Learn More <i className="fa-solid fa-arrow-right"></i>
+          <button className="bg-orange-500 text-white py-2 px-4 rounded hover:bg-blue-600 font-fira rounded-[25px]">
+            Plan Your Adventure <i className="fa-solid fa-arrow-right"></i>
           </button>
         </div>
       </div>
