89d32b2c3d72ff0dc7a0d7abc0f624d925a83327
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-28 21:30:01 +0300

    Import the herosection component and increase opacity

--- client/src/components/HomeHero/HomeHero.jsx
+++ client/src/components/HomeHero/HomeHero.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import bgImage from '../../assets/bg3.jpg';
-import HeroSection from '../Hero/Hero';
 import Navbar from '../Navbar/Navbar';
+import HeroSection from '../HeroSection/HeroSection';
 
 const HomeHero = () => {
     const [backgroundImage, setBackgroundImage] = useState(bgImage);
     
   return (
     <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className='bg-black bg-opacity-40'>
+      <div className='bg-black bg-opacity-60'>
         <Navbar />
         <HeroSection setBgImage={setBackgroundImage} />
       </div>
