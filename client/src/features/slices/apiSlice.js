de912d5ca6c48aa1fc51c7989c2e649002eb3357
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-30 21:24:06 +0300

    feat: Set up Redux Toolkit API integration

---
+++ client/src/features/slices/apiSlice.js
@@ -0,0 +1,12 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:8000",
+  credentials: "include",
+});
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["User"],
+  endpoints: () => ({}),
+});
