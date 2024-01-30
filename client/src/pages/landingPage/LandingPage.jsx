18be5c3c7581f7a6fe0421cd500cb9fb232d2bd5
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-30 21:22:41 +0300

    feat: Integrate components into LandingPage

---
+++ client/src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,26 @@
+import HomeHero from "../../components/HomeHero/HomeHero";
+import About from "../../components/about/about";
+import Features from "../../components/Featured/featured";
+import Explore from "../../components/Explore/explore";
+import PopularSection from "../../components/Popular/Popular";
+import Testimonial from "../../components/Testimonial/Testimonial";
+import BlogSection from "../../components/Blog/Blog";
+import Footer from "../../components/Footer/Footer";
+
+const LandingPage = () => {
+  
+  return (
+    <div className="bg-gray-100">
+      <HomeHero/>
+      <Features/>
+      <PopularSection/>
+      <About/>
+      <Explore/>
+      <Testimonial/>
+      <BlogSection/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default LandingPage;
