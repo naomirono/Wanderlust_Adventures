593e2473d4ec7034c73f2c391a60f8559835730d
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-31 18:56:58 +0300

    Update main.jsx to implement React Router for navigation and integrate Redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createRoutesFromElements,
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+} from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./pages/auth/Login.jsx";
+import Register from "./pages/auth/Register.jsx";
+import store from "./store.js";
+import { Provider } from "react-redux";
+import LandingPage from "./pages/landingPage/LandingPage.jsx";
+import ForgetPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgotpassword" element={<ForgetPassword />} />
+      <Route path="/resetpassword/:resetToken" element={<ResetPassword />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
