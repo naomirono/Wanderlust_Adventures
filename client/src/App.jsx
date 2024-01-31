20df591ba55de7c402952c3f4ed89036d9703907
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-31 18:55:34 +0300

    Refactor App component to handle user authentication and use React Router Outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,22 @@
-import HomeHero from "./components/HomeHero/HomeHero";
-import About from "./components/about/about";
-import Features from "./components/Featured/featured";
-import Explore from "./components/Explore/explore";
-import PopularSection from "./components/Popular/Popular";
-import Testimonial from "./components/Testimonial/Testimonial";
-import BlogSection from "./components/Blog/Blog";
-import Footer from "./components/Footer/Footer";
-
-function App() {
-  
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const App = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
   return (
-    <div className="bg-gray-100">
-      <HomeHero/>
-      <Features/>
-      <PopularSection/>
-      <About/>
-      <Explore/>
-      <Testimonial/>
-      <BlogSection/>
-      <Footer/>
-    </div>
+    <>
+      <Outlet />
+      <ToastContainer />
+    </>
   );
-}
+};
 
 export default App;
