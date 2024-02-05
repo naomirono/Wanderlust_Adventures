a9ceb7ad836b20725926a61c4f8f452de114fef3
Naomi Cherotich <naomirono1454@gmail.com>
2024-02-05 19:35:50 +0300

    Create a js file with blog mock data

---
+++ client/src/components/Blog/blogData.js
@@ -0,0 +1,26 @@
+export const blogPosts = [
+    {
+      title: 'Exploring the Wonders of Kenya: From Nairobi to Maasai Mara',
+      content: 'Embark on a captivating journey through Kenya, from the vibrant city of Nairobi to the breathtaking landscapes of Maasai Mara. Our travel experts share insider tips on experiencing the rich culture, diverse wildlife, and iconic landmarks that make Kenya an extraordinary destination',
+      author: 'Travel Explorer',
+      date: 'January 20, 2024',
+      time: '8 min',
+      profileImage: 'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      title: 'Safari Spectacle: Witnessing Kenya’s Majestic Wildlife in Amboseli',
+      content: 'Join us on an awe-inspiring safari adventure in Amboseli National Park, where the majestic Mount Kilimanjaro serves as a backdrop to incredible wildlife sightings. Our experienced guides reveal the wonders of Kenya’s natural beauty and biodiversity',
+      author: 'Safari Enthusiast',
+      date: 'January 15, 2024',
+      time: '7 min',
+      profileImage: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      title: 'Coastal Bliss: Luxury Retreats on Kenya’s Dazzling Indian Ocean Shores',
+      content: 'Experience the epitome of relaxation with our coastal retreat packages along Kenya’s Indian Ocean shores. From pristine beaches to vibrant coral reefs, discover the hidden gems of Kenya’s coastal paradise, complete with luxurious accommodations and cultural delights',
+      author: 'Sea Voyager',
+      date: 'January 10, 2024',
+      time: '9 min',
+      profileImage: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+    },
+  ];
