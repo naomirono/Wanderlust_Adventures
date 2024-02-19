8446af002f3ac68a37bb89e0a6c4cbda8bfa3e84
Naomi Cherotich <naomirono1454@gmail.com>
2024-02-19 21:26:01 +0300

    style: Hide specific divs on small devices in Features component

--- client/src/components/Featured/featured.jsx
+++ client/src/components/Featured/featured.jsx
@@ -3,40 +3,40 @@ import React from 'react';
 const Features = () => {
   return (
     <div className='relative'>
-    <div className="mx-auto max-w-[1000px] px-4 sm:px-6 lg:px-8 py-8 bg-blur-md backdrop-blur-md bg-white bg-opacity-30 absolute bottom-[-70px] left-0 right-0">
-      <div className="flex justify-around items-center">
+      <div className="mx-auto max-w-[1000px] px-4 sm:px-6 lg:px-8 py-8 bg-blur-md backdrop-blur-md bg-white bg-opacity-30 absolute bottom-[-70px] left-0 right-0">
+        <div className="flex justify-around items-center">
 
-        <div className="text-center">
-          <i className="fa-solid fa-globe text-orange-500 text-4xl mb-2"></i>
-          <p className="text-sm font-semibold">Global</p>
-          <p className="text-sm font-semibold">Destinations</p>
-        </div>
+          <div className="text-center">
+            <i className="fa-solid fa-globe text-orange-500 text-4xl mb-2"></i>
+            <p className="text-sm font-semibold">Global</p>
+            <p className="text-sm font-semibold">Destinations</p>
+          </div>
 
-        <div className="text-center">
-          <i className="fa-solid fa-money-check-dollar text-orange-500 text-4xl mb-2"></i>
-          <p className="text-sm font-semibold">Transparent</p>
-          <p className="text-sm font-semibold">Pricing</p>
-        </div>
+          <div className="text-center">
+            <i className="fa-solid fa-money-check-dollar text-orange-500 text-4xl mb-2"></i>
+            <p className="text-sm font-semibold">Transparent</p>
+            <p className="text-sm font-semibold">Pricing</p>
+          </div>
 
-        <div className="text-center">
-          <i className="fa-solid fa-face-smile text-orange-500 text-4xl mb-2"></i>
-          <p className="text-sm font-semibold">100% Satisfaction</p>
-          <p className="text-sm font-semibold">Guarantee</p>
-        </div>
+          <div className="text-center">
+            <i className="fa-solid fa-face-smile text-orange-500 text-4xl mb-2"></i>
+            <p className="text-sm font-semibold">100% Satisfaction</p>
+            <p className="text-sm font-semibold">Guarantee</p>
+          </div>
 
-        <div className="text-center">
-          <i className="fa-solid fa-user-tie text-orange-500 text-4xl mb-2"></i>
-          <p className="text-sm font-semibold">Professional & </p>
-          <p className="text-sm font-semibold">Qualified</p>
-        </div>
+          <div className="text-center hidden sm:block">
+            <i className="fa-solid fa-user-tie text-orange-500 text-4xl mb-2"></i>
+            <p className="text-sm font-semibold">Professional & </p>
+            <p className="text-sm font-semibold">Qualified</p>
+          </div>
 
-        <div className="text-center">
-          <i className="fa-solid fa-laptop text-orange-500 text-4xl mb-2"></i>
-          <p className="text-sm font-semibold">Online</p>
-          <p className="text-sm font-semibold">Booking</p>
+          <div className="text-center hidden sm:block">
+            <i className="fa-solid fa-laptop text-orange-500 text-4xl mb-2"></i>
+            <p className="text-sm font-semibold">Online</p>
+            <p className="text-sm font-semibold">Booking</p>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
