aaaf4620b1f71399adaab17752f36bf567e63e3f
Naomi Cherotich <naomirono1454@gmail.com>
2024-02-20 22:41:06 +0300

    refactor code and remove commented lines

--- client/src/components/HomeHero/HomeHero.jsx
+++ client/src/components/HomeHero/HomeHero.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import bgImage from '../../assets/bg3.jpg';
 import Navbar from '../Navbar/Navbar';
 import HeroSection from '../HeroSection/HeroSection';
 
 const HomeHero = () => {
-    const [backgroundImage, setBackgroundImage] = useState(bgImage);
-    
+  const [backgroundImage, setBackgroundImage] = useState(bgImage);
+
   return (
-    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className='bg-black bg-opacity-60'>
+    <div className="relative h-screen">
+      <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }} />
+      <div className="absolute inset-0 bg-black bg-opacity-60">
         <Navbar />
         <HeroSection setBgImage={setBackgroundImage} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeHero
+export default HomeHero;
